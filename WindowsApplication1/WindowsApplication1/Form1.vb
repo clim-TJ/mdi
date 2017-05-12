da3da5c2fda564e80cf6993737f42926e83652d5
clim-TJ <clim@traderjoes.com>
2017-05-11 20:23:02 -0700

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Dim frm2 As New child1
+        frm2.MdiParent = Me
+        frm2.Show()
+
+    End Sub
+End Class
